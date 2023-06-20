acaf5020d1370f228bce3b94f3221d67ca7ce63b
Esra <71944354+EsraAblak@users.noreply.github.com>
2023-06-20 12:47:18 +0300

    cmt

--- src/parser/map_controls.c
+++ src/parser/map_controls.c
@@ -2,27 +2,27 @@
 
 int	zero_location(char **split, int i, int j)
 {
-
 	if (i - 1 < 0 || i + 1 >= double_pointer_len(split) || j - 1 < 0 || j
 		+ 1 > (int)ft_strlen(split[i]) - 1)
-		    return (0);
-	if (split[i - 1][j] == ' ' || split[i + 1][j] == ' ' || split[i][j - 1] == ' '
-		|| split[i][j + 1] == ' ')
 		return (0);
-    if (split[i - 1][j] == '\0' || split[i + 1][j] == '\0' || split[i][j - 1] == '\0'
-		|| split[i][j + 1] == '\0')
+	if ((int)ft_strlen(split[i - 1]) < j)
+		return (0);
+	if (split[i - 1][j] == ' ' || split[i + 1][j] == ' ' || split[i][j
+		- 1] == ' ' || split[i][j + 1] == ' ')
+		return (0);
+	if (split[i - 1][j] == '\0' || split[i + 1][j] == '\0' || split[i][j
+		- 1] == '\0' || split[i][j + 1] == '\0')
 		return (0);
 	return (1);
 }
 
-int wall_check(t_cub *cub)
+int	wall_check(t_cub *cub)
 {
-    char **split;
+	char	**split;
+	int		i;
+	int		j;
 
-    split = ft_split(cub->map_reference,'\n');
-    int	i;
-	int	j;
-
+	split = ft_split(cub->map_reference, '\n');
 	i = 0;
 	j = 0;
 	while (split[i] != NULL)
@@ -30,39 +30,39 @@ int wall_check(t_cub *cub)
 		j = 0;
 		while (split[i][j] != '\0')
 		{
-			if (split[i][j] == '0' || split[i][j] == 'N' ||  split[i][j] == 'S'
-                ||  split[i][j] == 'W' ||  split[i][j] == 'E')
+			if (split[i][j] == '0' || split[i][j] == 'N' || split[i][j] == 'S'
+				|| split[i][j] == 'W' || split[i][j] == 'E')
 				if (!zero_location(split, i, j))
-                {
-                    free_double_pointer(split);
-                    return (1);
-                }
+				{
+					free_double_pointer(split);
+					return (1);
+				}
 			j++;
 		}
 		i++;
 	}
-    free_double_pointer(split);
-    return (0);
+	free_double_pointer(split);
+	return (0);
 }
 
-int player_count(t_cub *cub)
+int	player_count(t_cub *cub)
 {
-    int i;
-    int count;
+	int i;
+	int count;
 
-    i = 0;
-    count = 0;
-    while(cub->map_reference[i])
-    {
-        if (cub->map_reference[i] == 'N' || cub->map_reference[i] == 'W'
-            || cub->map_reference[i] == 'S' || cub->map_reference[i] == 'E')
-                count++;
-        i++;
-    }
-    if (count != 1)
-    {
-        printf("player count\n");
-        return (1);
-    }
-    return (0);
+	i = 0;
+	count = 0;
+	while (cub->map_reference[i])
+	{
+		if (cub->map_reference[i] == 'N' || cub->map_reference[i] == 'W'
+			|| cub->map_reference[i] == 'S' || cub->map_reference[i] == 'E')
+			count++;
+		i++;
+	}
+	if (count != 1)
+	{
+		printf("player count\n");
+		return (1);
+	}
+	return (0);
 }
